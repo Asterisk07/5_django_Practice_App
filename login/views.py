3c10652ab3bbc88ee587e5e95b283c93dfb29c57
Asterisk07 <ferbandphineas100@gmail.com>
2023-07-05 00:29:09 +0530

    switched from django -sendfile to FileResponse

--- login/views.py
+++ login/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render, redirect
 from .models import Person
 import os
 
-from django.http import HttpResponse
-from sendfile import sendfile
+# from django.http import HttpResponse
+# from sendfile import sendfile
+
+from django.http import FileResponse, Http404
 
 def serve_pdf(request,file1):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     pdf_path = os.path.join(parent_dir,'media')
     pdf_path = os.path.join(pdf_path,file1+".pdf")
-    return sendfile(request, pdf_path, attachment=True)
+    return FileResponse(open(pdf_path, 'rb'), content_type='application/pdf')
+
+# def serve_pdf(request,file1):
+#     current_dir = os.path.dirname(os.path.abspath(__file__))
+#     parent_dir = os.path.dirname(current_dir)
+#     pdf_path = os.path.join(parent_dir,'media')
+#     pdf_path = os.path.join(pdf_path,file1+".pdf")
+#     return sendfile(request, pdf_path, attachment=True)
+
+
 
 
 def login(request):
